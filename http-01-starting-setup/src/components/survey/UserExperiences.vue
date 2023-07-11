db01280b3dc4cd50744de7fbf1ea19f93c1fb4a8
tobi <t.goral@gmx.net>
2023-07-11 13:45:56 +0200

    ok

---
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -0,0 +1,71 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p  v-if="results.length==0">No stored data found</p>
+      <ul v-if="!isLoading && results.length>0">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading=true;
+      fetch('https://dummy-33dd0-default-rtdb.firebaseio.com/surveys.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading=false;
+
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted(){
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
