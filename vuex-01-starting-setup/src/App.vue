1b6c4bec3663d3b0ed7af71ebe153bb94ebc63fb
tobi <t.goral@gmx.net>
2023-07-14 11:15:33 +0200

    ok

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ this.$store.state.counter }}</h3>
+    <button @click="addOne">Add 1</button>
+    <the-counter></the-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue'
+export default {
+  components: {
+    BaseContainer,TheCounter
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
